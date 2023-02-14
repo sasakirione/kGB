f3699ab10e595082ec1c7cbf2781267109224c0b
sasakirione <sasakirione@gmail.com>
2023-02-15 01:16:23 +0900

    Add: メモリの実装
    エコーメモリってなに？

--- src/main/kotlin/Chipset.kt
+++ src/main/kotlin/Chipset.kt
@@ -1,11 +1,43 @@
+@OptIn(ExperimentalUnsignedTypes::class)
 class Chipset {
+    /**
+     * メモリ
+     */
+    private var memory: UByteArray = UByteArray(0x2000)
+    /**
+     * Highメモリ
+     */
+    private var highMemory: UByteArray = UByteArray(0x7f)
+
+    /**
+     * 16bitアドレスから値を読み出す
+     *
+     * @param address 16bitアドレス
+     * @return アドレスに格納されている値
+     */
     fun getValue(address: UShort): UByte {
         println(address)
-        return 0x00u
+        val value = when (address) {
+            in 0xc000u..0xdfffu -> memory[address.toInt() - 0xc000]
+            in 0xff80u..0xfffeu -> highMemory[address.toInt() - 0xff80]
+            else -> 0xffu
+        }
+        return value
     }
 
+    /**
+     * 16bitアドレスに値を書き込む
+     *
+     * @param address 16bitアドレス
+     * @param sourceValue 書き込む値
+     */
     fun setValue(address: UShort, sourceValue: UByte) {
         println(address)
         println(sourceValue)
+        when (address) {
+            in 0xc000u..0xdfffu -> memory[address.toInt() - 0xc000] = sourceValue
+            in 0xff80u..0xfffeu -> highMemory[address.toInt() - 0xff80] = sourceValue
+            else -> {}
+        }
     }
 }
