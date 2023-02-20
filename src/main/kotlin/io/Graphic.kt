8160be22a836d4243ff6748dee211bc28f2f9574
sasakirione <sasakirione@gmail.com>
2023-02-20 23:50:53 +0900

    よくわからん

--- src/main/kotlin/io/Graphic.kt
+++ src/main/kotlin/io/Graphic.kt
@@ -7,11 +7,11 @@ object Graphic {
     /**
      * スクリーンサイズ横
      */
-    private val SCREEN_WIDTH = 160
+    val SCREEN_WIDTH = 160
     /**
      * スクリーンサイズ縦
      */
-    private val SCREEN_HEIGHT = 144
+    val SCREEN_HEIGHT = 144
 
     /**
      * VRAM
@@ -21,6 +21,19 @@ object Graphic {
      * OAM
      */
     private var oam: UByteArray = UByteArray(0xa0)
+    /**
+     * LCDコントロール
+     */
+    private var lcdControl: UByte = 0x80u
+    /**
+     * 現在モードでの経過クロック数
+     */
+    private var currentModeClock: UByte = 0u
+
+    /**
+     * グラフィック領域のイメージバッファ
+     */
+    var imageBuffer: UByteArray = UByteArray(SCREEN_WIDTH * SCREEN_HEIGHT)
     /**
      * VRAMステータス(ゲームボーイだとこれで書き込みと読み出しが合致会わないようにしている)
      */
@@ -74,6 +87,58 @@ object Graphic {
                 Logger.warn("存在しないGraphicIO領域への書き込みです。")
             }
         }
+    }
+
+    /**
+     * 画面を更新する(1フレーム)
+     *
+     * @param tick タイマーのカウント値
+     */
+    fun update(tick: UByte) {
+        // LCDがオフの場合は何もしない
+        if (lcdControl.toInt() and 0x80 == 0) {
+            return
+        }
+
+        // 現在のモードを取得
+        val currentMode = vramStatus.toInt() and 0x3
+
+        // 現在のモードでの経過クロック数を更新
+        currentModeClock = (tick + currentModeClock).toUByte()
+
+        // 現在のモードでの経過クロック数がモードによって異なる場合はモードを変更する
+        when (currentMode) {
+            // H-Blank
+            0 -> {
+                if (currentModeClock.toInt() >= 204) {
+                    currentModeClock = 0u
+                    vramStatus = (vramStatus.toInt() and 0xfc or 0x2).toUByte()
+                }
+            }
+            // V-Blank
+            1 -> {
+                if (currentModeClock.toInt() >= 456) {
+                    currentModeClock = 0u
+                    vramStatus = (vramStatus.toInt() and 0xfc or 0x3).toUByte()
+                }
+            }
+            // OAM
+            2 -> {
+                if (currentModeClock.toInt() >= 80) {
+                    currentModeClock = 0u
+                    vramStatus = (vramStatus.toInt() and 0xfc or 0x0).toUByte()
+                }
+            }
+            // VRAM
+            3 -> {
+                if (currentModeClock.toInt() >= 172) {
+                    currentModeClock = 0u
+                    vramStatus = (vramStatus.toInt() and 0xfc or 0x0).toUByte()
+                }
+            }
+        }
+
+
     }
 
 }
