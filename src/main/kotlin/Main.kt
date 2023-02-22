0c036338238280858c273b3d81a41acd950377b8
sasakirione <sasakirione@gmail.com>
2023-02-22 09:34:05 +0900

    Re

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,11 @@ fun main() {
     val chipset = Chipset("pokemon_red")
     val cpu = Cpu(chipset)
 
+    val oneFlame = 456 * (144 + 10)
     while (true) {
         var step = 0
-        while (true) {
+        while (step < oneFlame) {
             step += cpu.step()
-            if (step >= 456 * (144 + 10)) {
-                break
-            }
         }
     }
 }
